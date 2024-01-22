32bb86acf94d2986a1473db43b42cf2d5fb94546
mgckaled <marcelgckaled@gmail.com>
2024-01-22 11:36:42 -0300

    feat: add read routes - list of tasks and return task by id

--- app.py
+++ app.py
@@ -5,25 +5,69 @@ from models.task import Task
 app = Flask(__name__)
 
 tasks = []
-task_id_control = 1
+TASK_ID_CONTROL = 1
 
 
 @app.route("/")
 def init_server():
+    """
+    Rota inicial que verifica se o servidor está em execução.
+
+    Returns:
+        str: Mensagem indicando que o servidor está em execução.
+    """
     return "Server is running"
 
 
 @app.route("/tasks", methods=["POST"])
 def create_task():
-    global task_id_control
+    """
+    Rota para criar uma nova tarefa.
+
+    Returns:
+        jsonify: Resposta JSON indicando se a tarefa foi criada com sucesso.
+    """
+    global TASK_ID_CONTROL
     data = request.get_json()
     new_task = Task(
-        id=task_id_control, title=data["title"], description=data.get("description", "")
+        id=TASK_ID_CONTROL, title=data["title"], description=data.get("description", "")
     )
-    task_id_control += 1
+    TASK_ID_CONTROL += 1
     tasks.append(new_task)
-    print(tasks)
+    print(tasks)  # Remova ou substitua por logging, se necessário.
     return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    """
+    Rota para obter a lista de todas as tarefas.
+
+    Returns:
+        jsonify: Resposta JSON contendo a lista de tarefas e o total de tarefas.
+    """
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {"tasks": task_list, "total_tasks": len(task_list)}
+    return jsonify(output)
+
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task(id):
+    """
+    Rota para obter detalhes de uma tarefa específica.
+
+    Args:
+        id (int): O identificador único da tarefa.
+
+    Returns:
+        jsonify: Resposta JSON contendo detalhes da tarefa ou uma mensagem de erro se não for encontrada.
+    """
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 
 if __name__ == "__main__":
--- models/task.py
+++ models/task.py
@@ -1,11 +1,30 @@
 class Task:
+    """
+    Representa uma tarefa com atributos como identificador, título, descrição e status de conclusão.
+    """
+
     def __init__(self, id, title, description, completed=False) -> None:
+        """
+        Inicializa uma nova instância da classe Task.
+
+        Args:
+            id (int): O identificador único da tarefa.
+            title (str): O título da tarefa.
+            description (str): A descrição da tarefa.
+            completed (bool, optional): Indica se a tarefa está concluída. Padrão é False.
+        """
         self.id = id
         self.title = title
         self.description = description
         self.completed = completed
 
     def to_dict(self):
+        """
+        Converte a instância da classe Task em um dicionário.
+
+        Returns:
+            dict: Um dicionário contendo os atributos da tarefa.
+        """
         return {
             "id": self.id,
             "title": self.title,
